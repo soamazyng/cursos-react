40aec6a438518e3d7d3fff1267d6767ebc802427
Jay Benedicto <soamazing@gmail.com>
2020-03-04 21:07:40 -0300

    fim do módulo introdução ao react

--- rocketseat_primeiro_projeto_react/modulo04/src/components/TechList.js
+++ rocketseat_primeiro_projeto_react/modulo04/src/components/TechList.js
@@ -1,11 +1,43 @@
-import React, {Component} from 'react'
+import React, {Component} from 'react';
+
+import TechItem from './TechItem';
 
 class TechList extends Component {
 
+  // define as defaultsProps do componente de classe
+  static defaultProps = {
+
+  };
+
+  // define as propTypes do componente de classe
+  static propTypes = {
+
+  };
+
   state = {
-    techs: ['node', 'react', 'reactnative'],
+    techs: [],
     newTech: ''
   };
+
+  // Executado assim que o componente aparece em tela
+  componentDidMount(){
+    const techs = localStorage.getItem('techs');
+    if(techs){
+      this.setState({techs: JSON.parse(techs)})
+    }
+  }
+
+  // Executado sempre que houver alterações nas props ou estado
+  componentDidUpdate(prevProps, prevState){
+    if (prevState.techs !== this.state.techs){
+      localStorage.setItem('techs', JSON.stringify(this.state.techs));
+    }
+  }
+
+  // Executado quando o componente deixa de existir
+  componentWillUnmount(){
+    
+  }
 
   // toda vez que vc precisar utilizar o this deve criar uma função do tipo Arrow Function
   handleInputChange = e => {
@@ -24,12 +56,21 @@ class TechList extends Component {
     });
   }
 
+  handleDelete = (tech) => {
+    this.setState({techs: this.state.techs.filter(t => t !== tech)})
+  }
+
   render(){
     return (
       // esta tag vazia é chamada de fragment é uma tag vazia para poder utilizar mais tags em um componente
       <>
         <ul>
-          {this.state.techs.map(tech => <li key={tech}>{tech}</li>)}
+          {this.state.techs.map(tech => 
+            <TechItem 
+              key={tech} 
+              tech={tech} 
+              onDelete={() => this.handleDelete(tech)} />)
+          }
         </ul>
         <form onSubmit={this.handleSubmit}>
         <input 
