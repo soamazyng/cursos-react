e31a1fa93b8a0a3dce59f6b249a6801456f00feb
Jay Benedicto <soamazing@gmail.com>
2020-03-09 22:15:50 -0300

    modulo05

---
+++ rocketseat_primeiro_projeto_com_reactjs/modulo05/README.md
@@ -0,0 +1,48 @@
+# Anotações
+
+## Criando um projeto novo
+
+Não é preciso instalar Babel ou Webpack pois o React possui um CLI que contém tudo que é necessário para rodar um projeto novo.
+
+E já vem com todas as configurações prontas
+Para isso basta rodar o comando abaixo:
+
+```javascript
+npx create-react-app <name>
+```
+
+## Configuração do ESLint e Pretter :
+
+```
+  npm i eslint --save-dev
+```
+
+Depois de instalar o eslint
+
+```
+npx eslint --init
+```
+
+Agora instalar as dependências do prettier
+
+```
+ npm i prettier eslint-config-prettier eslint-plugin-prettier babel-eslint --save-dev
+```
+
+## Trabalhando com Rotas dentro do React
+
+```
+npm i react-router-dom
+```
+
+## Trabalhando com Styled Component
+
+```
+npm i styled-component
+```
+
+## Pacote de ícones famosos
+
+```
+npm i react-icons
+```
---
+++ rocketseat_primeiro_projeto_com_reactjs/modulo05/src/pages/Main/index.js
@@ -0,0 +1,71 @@
+import React, { Component } from 'react';
+
+import { FaGithubAlt, FaPlus, FaSpinner } from 'react-icons/fa';
+
+import api from '../../services/api';
+
+import { Container, Form, SubmitButton } from './styles';
+
+class Main extends Component {
+  state = {
+    newRepo: '',
+    repositories: [],
+    loadingButton: false,
+  };
+
+  handleInputChange = e => {
+    this.setState({ newRepo: e.target.value });
+  };
+
+  handleSubmit = async e => {
+    e.preventDefault();
+
+    const { newRepo, repositories } = this.state;
+
+    this.setState({ loadingButton: true });
+
+    const response = await api.get(`/repos/${newRepo}`);
+
+    const data = {
+      name: response.data.full_name,
+    };
+
+    this.setState({
+      repositories: [...repositories, data],
+      newRepo: '',
+      loadingButton: false,
+    });
+  };
+
+  render() {
+    const { newRepo, loadingButton } = this.state;
+
+    return (
+      <>
+        <Container>
+          <h1>
+            <FaGithubAlt />
+            Repositórios
+          </h1>
+          <Form onSubmit={this.handleSubmit}>
+            <input
+              type="text"
+              placeholder="Adicionar repositório"
+              value={newRepo}
+              onChange={this.handleInputChange}
+            />
+            <SubmitButton loadingButton={loadingButton}>
+              {loadingButton ? (
+                <FaSpinner color="#fff" size={14} />
+              ) : (
+                <FaPlus color="#fff" size={14} />
+              )}
+            </SubmitButton>
+          </Form>
+        </Container>
+      </>
+    );
+  }
+}
+
+export default Main;
