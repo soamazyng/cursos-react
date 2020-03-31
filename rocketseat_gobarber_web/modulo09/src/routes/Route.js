652cd724cf8b1b583dafe97006d05ae5fe61548b
Jay Benedicto <soamazing@gmail.com>
2020-03-31 18:46:36 -0300

    modulo 9

---
+++ rocketseat_gobarber_web/modulo09/src/routes/Route.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Route, Redirect } from 'react-router-dom';
+
+import PropTypes from 'prop-types';
+
+export default function RouteWrapper({
+  component: Component,
+  isPrivate,
+  ...rest
+}) {
+  const signed = false;
+
+  if (!signed && isPrivate) {
+    return <Redirect to="/" />;
+  }
+
+  if (signed && !isPrivate) {
+    return <Redirect to="/dashboard" />;
+  }
+
+  return <Route {...rest} component={Component} />;
+}
