4a5e25e5fe98ddc0e8ff929cd0db239abbb3adf0
Jay Benedicto <soamazing@gmail.com>
2020-04-02 18:24:14 -0300

    modulo 9

--- rocketseat_gobarber_web/modulo09/src/pages/SignIn/index.js
+++ rocketseat_gobarber_web/modulo09/src/pages/SignIn/index.js
@@ -1,9 +1,35 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
-export default function componentName() {
+import { Form, Input } from '@rocketseat/unform';
+
+import * as Yup from 'yup';
+
+import logo from '~/assets/logo.svg';
+
+const schema = Yup.object().shape({
+  email: Yup.string()
+    .email('Insira um e-mail válido')
+    .required('O e-mail é obrigatório'),
+  password: Yup.string().required('A senha é obrigatória'),
+});
+
+export default function SignIn() {
+  function handleSubmit(data) {
+    console.tron.log(data);
+  }
+
   return (
     <>
-      <h1>login</h1>
+      <img src={logo} alt="GoBarber" />
+
+      <Form schema={schema} onSubmit={handleSubmit}>
+        <Input name="email" type="email" placeholder="Seu e-mail" />
+        <Input name="password" type="password" />
+
+        <button type="submit">Acessar</button>
+        <Link to="/register">Criar conta gratuita</Link>
+      </Form>
     </>
   );
 }
