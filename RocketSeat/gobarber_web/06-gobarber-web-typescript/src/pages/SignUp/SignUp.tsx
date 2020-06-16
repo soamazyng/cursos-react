a19f01df20117fc45119aadb0758d14e28cc82f9
Jaqueline Benedicto <soamazing@gmail.com>
2020-06-16 15:02:00 -0300

    fix folder

---
+++ RocketSeat/gobarber_web/06-gobarber-web-typescript/src/pages/SignUp/SignUp.tsx
@@ -0,0 +1,113 @@
+import React, { useCallback, useRef, useState } from 'react';
+import { FiArrowLeft, FiUser, FiMail, FiLock } from 'react-icons/fi';
+import { FormHandles } from '@unform/core';
+import { Form } from '@unform/web';
+import * as Yup from 'yup';
+import { Link, useHistory } from 'react-router-dom';
+
+import getValidationErrors from '../../utils/getValidationErrors';
+
+import logoImg from '../../assets/logo.svg';
+
+import { useToast } from '../../hooks/toast';
+
+import Input from '../../components/Input/input';
+import Button from '../../components/Button/button';
+
+import api from '../../services/api';
+
+import { Container, Content, Background } from './SignUp.Style';
+
+interface SignUpFormData {
+  name: string;
+  email: string;
+  password: string;
+}
+
+const SignUp: React.FC = () => {
+  const [loading, setLoading] = useState(false);
+  // recupera o contexto
+  const { addToast } = useToast();
+  const history = useHistory();
+
+  const formRef = useRef<FormHandles>(null);
+
+  // envia o formulário
+  const handleSubmit = useCallback(
+    async (data: SignUpFormData) => {
+      formRef.current?.setErrors({});
+      setLoading(true);
+      try {
+        const schema = Yup.object().shape({
+          name: Yup.string().required('Nome obrigatório'),
+          email: Yup.string()
+            .required('E-mail obrigatório')
+            .email('E-mail deve ser válido'),
+          password: Yup.string().min(6, 'Deve ter no minímo de 6 digitos'),
+        });
+
+        await schema.validate(data, {
+          abortEarly: false,
+        });
+
+        // cadastra
+        await api.post('/users', data);
+        addToast({
+          type: 'success',
+          title: 'Cadastro realizado com sucesso.',
+          description: 'Você já pode fazer seu logon no GoBarber',
+        });
+        history.push('/');
+        setLoading(false);
+      } catch (error) {
+        if (error instanceof Yup.ValidationError) {
+          const errors = getValidationErrors(error);
+          formRef.current?.setErrors(errors);
+          return;
+        }
+
+        // toast será disparado aqui
+        addToast({
+          type: 'error',
+          title: 'Erro no cadastro',
+          description: 'Ocorreu um erro ao realizar o cadastro.',
+        });
+      } finally {
+        setLoading(false);
+      }
+    },
+    [addToast, history, setLoading],
+  );
+
+  return (
+    <Container>
+      <Background />
+      <Content>
+        <img src={logoImg} alt="GoBarber" />
+
+        <Form ref={formRef} onSubmit={handleSubmit}>
+          <h1>Faça seu Cadastro</h1>
+          <Input name="name" placeholder="Nome" icon={FiUser} />
+          <Input name="email" type="email" placeholder="E-mail" icon={FiMail} />
+          <Input
+            name="password"
+            type="password"
+            placeholder="Senha"
+            icon={FiLock}
+          />
+
+          <Button loading={loading} type="submit">
+            Cadastrar
+          </Button>
+        </Form>
+
+        <Link to="/">
+          <FiArrowLeft />
+          Voltar para Logon
+        </Link>
+      </Content>
+    </Container>
+  );
+};
+
+export default SignUp;
---
+++ RocketSeat/gobarber_web/06-gobarber-web-typescript/src/routes/index.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Switch } from 'react-router-dom';
+
+import SignIn from '../pages/SignIn/SignIn';
+import SignUp from '../pages/SignUp/SignUp';
+import ForgotPassword from '../pages/ForgotPassword/ForgotPassword';
+import ResetPassword from '../pages/ResetPassword/ResetPassword';
+import Dashboard from '../pages/Dashboard/Dashboard';
+
+import Route from './Route';
+
+const Routes: React.FC = () => (
+  <Switch>
+    <Route path="/" exact component={SignIn} />
+    <Route path="/signup" exact component={SignUp} />
+    <Route path="/forgot-password" exact component={ForgotPassword} />
+    <Route path="/reset-password" exact component={ResetPassword} />
+    <Route path="/dashboard" exact isPrivate component={Dashboard} />
+  </Switch>
+);
+
+export default Routes;
