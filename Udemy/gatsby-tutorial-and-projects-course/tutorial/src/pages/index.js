af156767a187a35278b005bd8e7d527fa2fcabe7
Jaqueline Benedicto <soamazing@gmail.com>
2020-06-19 19:35:47 -0300

    first commit

---
+++ Udemy/gatsby-tutorial-and-projects-course/tutorial/src/pages/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Layout from '../components/Layout';
+
+export default () => (
+  <Layout>
+    <h1 style={{ color: 'red', textTransform: 'uppercase' }}>
+      hello from gatsby
+    </h1>
+  </Layout>
+);
